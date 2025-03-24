1735dc50b2f136feaa13bb247b619fb689fffd54
Stick <20114698+stickms@users.noreply.github.com>
2025-03-24 17:08:45 -0400

    Updated zip lookup to return location zip and city name (TODO: cleanup code)

--- src/index.ts
+++ src/index.ts
@@ -15,7 +15,7 @@ app.get('/', async (req, res) => {
   });
 });
 
-app.get('/lookup-zip', async (req, res) => {
+app.post('/lookup-zip', async (req, res) => {
   const { location } = req.body;
 
   const api_uri = 'https://maps.googleapis.com/maps/api/geocode/json';
@@ -34,10 +34,47 @@ app.get('/lookup-zip', async (req, res) => {
   const zip_code = Object.values(components).find((x) =>
     x['types']?.includes('postal_code')
   );
-  
-  res.json({
-    zip_code: zip_code['long_name']
-  });
+
+  if (!zip_code) {
+    // Reverse lookup time
+    const geometry = Object.values(json['results']).find(
+      (x) => !!x['geometry']
+    )['geometry'];
+
+    // This gives us lat & long
+    const location = geometry['location'];
+
+    const loc_resp = await fetch(
+      `${api_uri}?latlng=${encodeURIComponent(`${location['lat']},${location['lng']}`)}&key=${GOOGLE_API_KEY}`
+    );
+
+    const loc_json = await loc_resp.json();
+
+    const res_json = { zip_code: '', city: '' };
+
+    for (const result of loc_json['results']) {
+      for (const cat of result['address_components']) {
+        if (cat['types'].includes('postal_code')) {
+          res_json['zip_code'] = cat['long_name'];
+        }
+
+        if (cat['types'].includes('locality')) {
+          res_json['city'] = cat['long_name'];
+        }
+      }
+    }
+
+    res.json(res_json);
+  } else {
+    const city = Object.values(components).find((x) =>
+      x['types']?.includes('locality')
+    );
+
+    res.json({
+      zip_code: zip_code['long_name'],
+      city: city['long_name']
+    });
+  }
 });
 
 app.get('/convenience-zones', async (req, res) => {
