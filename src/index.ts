b5efc7da0031a726d5fd8357921d7bd1ba79ba1d
stickms <20114698+stickms@users.noreply.github.com>
2025-04-06 17:46:48 -0400

    Added patterns endpoints

--- src/index.ts
+++ src/index.ts
@@ -140,103 +140,70 @@ app.post('/convenience-zones', async (req, res) => {
   });
 });
 
+app.post('/patterns', async (req, res) => {
+  const { cz_id, patterns, papdata } = req.body;
+
+  const czone_id = +cz_id;
+
+  const papdata_obj = await prisma.paPData.create({
+    data: {
+      papdata: papdata,
+      czone_id: czone_id
+    }
+  });
+
+  const patterns_obj = await prisma.movementPattern.create({
+    data: {
+      patterns: patterns,
+      start_date: new Date(),
+      czone_id: czone_id
+    }
+  });
+
+  res.json({
+    data: {
+      papdata: {
+        id: papdata_obj.id
+      },
+      patterns: {
+        id: patterns_obj.id
+      }
+    }
+  });
+});
+
+app.get('/patterns/:czone_id', async (req, res) => {
+  const czone_id = +req.params.czone_id;
+
+  const papdata_obj = await prisma.paPData.findUnique({
+    where: {
+      czone_id: czone_id
+    }
+  });
+
+  const patterns_obj = await prisma.movementPattern.findUnique({
+    where: {
+      czone_id: czone_id
+    }
+  });
+
+  if (!papdata_obj || !patterns_obj) {
+    res.status(404).json({
+      message: 'Could not find patterns or papdata'
+    });
+
+    return;
+  }
+
+  res.json({
+    data: {
+      papdata: JSON.parse(papdata_obj.papdata),
+      patterns: JSON.parse(patterns_obj.patterns)
+    }
+  });
+});
+
 const port = 1890;
 app.listen(port, () => {
   console.log(`Server is listening on port ${port}`);
 });
-
-/* previous implementation commented out */
-
-// app.post('/lookup-zip', async (req, res) => {
-//   const { location } = req.body;
-
-//   const api_uri = 'https://maps.googleapis.com/maps/api/geocode/json';
-
-//   const resp = await fetch(
-//     `${api_uri}?address=${encodeURIComponent(location)}&key=${GOOGLE_API_KEY}`
-//   );
-
-// TODO: Fix typing issues
-
-/* previous implementation commented out */
-
-//   const json = await resp.json();
-//   const components = Object.values(json['results']).find(
-//     (x) => !!x['address_components']
-//   )['address_components'];
-
-//   const zip_code = Object.values(components).find((x) =>
-//     x['types']?.includes('postal_code')
-//   );
-
-//   if (!zip_code) {
-//     // Reverse lookup time
-//     const geometry = Object.values(json['results']).find(
-//       (x) => !!x['geometry']
-//     )['geometry'];
-
-//     // This gives us lat & long
-//     const location = geometry['location'];
-
-//     const loc_resp = await fetch(
-//       `${api_uri}?latlng=${encodeURIComponent(`${location['lat']},${location['lng']}`)}&key=${GOOGLE_API_KEY}`
-//     );
-
-//     const loc_json = await loc_resp.json();
-
-//     const res_json = { zip_code: '', city: '' };
-
-//     for (const result of loc_json['results']) {
-//       for (const cat of result['address_components']) {
-//         if (cat['types'].includes('postal_code')) {
-//           res_json['zip_code'] = cat['long_name'];
-//         }
-
-//         if (cat['types'].includes('locality')) {
-//           res_json['city'] = cat['long_name'];
-//         }
-//       }
-//     }
-
-//     res.json(res_json);
-//   } else {
-//     const city = Object.values(components).find((x) =>
-//       x['types']?.includes('locality')
-//     );
-
-//     res.json({
-//       zip_code: zip_code['long_name'],
-//       city: city['long_name']
-//     });
-//   }
-// });
-
-// app.get('/convenience-zones', async (req, res) => {
-//   const zones = await prisma.convenienceZone.findMany();
-//   res.json({
-//     data: zones
-//   });
-// });
-
-// app.post('/convenience-zones', async (req, res) => {
-//   const { name, label, latitude, longitude, cbg_list, size } = req.body;
-
-//   const zone = await prisma.convenienceZone.create({
-//     data: {
-//       name,
-//       label,
-//       latitude,
-//       longitude,
-//       cbg_list,
-//       size
-//     }
-//   });
-
-//   res.json({
-//     data: zone
-//   });
-// });
-
-// const port = 1890;
-// app.listen(port);
-// console.log(`Server is listening on port ${port}`);
